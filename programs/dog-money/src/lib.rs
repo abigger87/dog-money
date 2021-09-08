2d0f5219dd96dd9622ca459fbaf9485ee20aa41f
Andreas Bigger <abigger@nascent.xyz>
2021-09-07 23:27:23 -0700

    dog-money stash

--- programs/dog-money/src/lib.rs
+++ programs/dog-money/src/lib.rs
@@ -4,7 +4,7 @@ use anchor_spl::token::{self, Mint, MintTo, TokenAccount, Transfer};
 #[program]
 pub mod dog_money {
     use super::*;
-    pub fn initialize_user(ctx: Context<InitializeUser>, amount: u64) -> ProgramResult {
+    pub fn initialize_user(ctx: Context<InitializeUser>, amount: u64, nonce: u8) -> ProgramResult {
         let user_data = &mut ctx.accounts.user_data;
         user_data.first_deposit = ctx.accounts.clock.unix_timestamp;
 
@@ -17,7 +17,18 @@ pub mod dog_money {
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::transfer(cpi_ctx, amount)?;
 
-        msg!("time {}", user_data.first_deposit);
+        // ** Mint 1000x to user
+        let dog_money_amount = amount.checked_mul(1000).unwrap();
+        let seeds = &[ctx.accounts.usdc_mint.to_account_info().as_ref(), &[ctx.accounts.usdc_mint.to_account_info().nonce]];
+        let signer = &[&seeds[..]];
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.dog_money_mint.to_account_info(),
+            to: ctx.accounts.user_dog_money.to_account_info(),
+            authority: ctx.accounts.program_signer.clone(),
+        };
+        let cpi_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+        token::mint_to(cpi_ctx, dog_money_amount)?;
         Ok(())
     }
 }
@@ -29,11 +40,17 @@ pub struct InitializeUser<'info> {
     user_data: ProgramAccount<'info, UserData>,
     #[account(signer)]
     authority: AccountInfo<'info>,
+    #[account(mut)]
     usdc_mint: CpiAccount<'info, Mint>,
+    #[account(mut)]
     user_usdc: CpiAccount<'info, TokenAccount>,
+    #[account(mut)]
     program_vault: CpiAccount<'info, TokenAccount>,
+    #[account(mut)]
     dog_money_mint: CpiAccount<'info, Mint>,
+    #[account(mut, "user_dog_money.owner == *authority.key")]
     user_dog_money: CpiAccount<'info, TokenAccount>,
+    #[account(executable, "token_program.key == &token::ID")]
     token_program: AccountInfo<'info>,
     rent: Sysvar<'info, Rent>,
     system_program: AccountInfo<'info>,
--- tests/dog-money.js
+++ tests/dog-money.js
@@ -1,4 +1,6 @@
 const anchor = require('@project-serum/anchor');
+const assert = require('assert');
+// const { assert } = require('chai');
 const {
   TOKEN_PROGRAM_ID,
   getTokenAccount,
@@ -26,23 +28,27 @@ describe('dog-money', () => {
 
     // ** Program Signer PDA (Program Derived Address) - signs transactions for the program
     const [_programSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
-      [dataAccount.Keypair.keypair.publicKey.toBuffer()],
+      [usdcMint.toBuffer()],
       program.programId
     );
     programSigner = _programSigner;
 
     // ** Associated account PDA - store user data
-    const userData = await program.account.userData.associatedAddress.fetch(program.provider.wallet.PublicKey, usdcMint);
+    const userData = await program.account.userData.associatedAddress.fetch(
+      program.provider.wallet.PublicKey,
+      usdcMint
+    );
 
     // ** Create user USDC account
+    const amount = new anchor.BN(5 * 10 ** 6);
     userUsdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
-    await mintToAccount(program.provider, usdcMint, userUsdc, new anchor.BN(5 * 10 ** 6), program.provider.wallet.publicKey);
-    
+    await mintToAccount(program.provider, usdcMint, userUsdc, amount, program.provider.wallet.publicKey);
+
     programVault = await createTokenAccount(program.provider, usdcMint, program.programId);
     dogMoneyMint = await createMint(program.provider, programSigner);
     userDogMoney = await createTokenAccount(program.provider, dogMoneyMint, program.provider.wallet.publicKey);
 
-    await program.rpc.initializeUser({
+    await program.rpc.initializeUser(amount, {
       Accounts: {
         programSigner,
         userData,
@@ -52,10 +58,18 @@ describe('dog-money', () => {
         programVault,
         dogMoneyMint,
         userDogMoney,
+        tokenProgram: TOKEN_PROGRAM_ID,
         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
         systemProgram: anchor.web3.SystemProgram.programId,
         clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
       },
     });
+
+    // ** Assert we sent the money
+    userUsdcData = await getTokenAccount(program.provider, userUsdc);
+    assert.ok(userUsdcData.amount.eq(new anchor.BN(0)));
+
+    userDogMoneyData = await getTokenAccount(program.provider, userDogMoney);
+    assert.ok(userDogMoneyData.amount.eq(amount.mul(new anchor.BN(1000))));
   });
 });
